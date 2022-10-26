9d7f47e3e3c5e9586a601510af24888cdcadf6b2
mampionona33 <ramamps33@gmail.com>
2022-10-26 11:56:50 +0300

    finish

--- script/getEnText.js
+++ script/getEnText.js
@@ -15,7 +15,7 @@
   if (anchorNode.tagName.match(/li/gi)) {
     console.log(anchorNode.tagName);
     formatList = DOT + ' ' + selectedTextToString;
-    formatList = formatList.replace(/\n/g, '\n' + DOT + ' ');
+    outPutText = formatList.replace(/\n/g, '\n' + DOT + ' ');
   } else {
     if (selectedTextToString.length > 0) {
       outPutText = outPutText.concat(selectedTextToString, '\n');
@@ -23,30 +23,23 @@
   }
 
   chrome.storage.sync.get(['En_text'], (result) => {
+    // If there is a stored data
     if (Object.keys(result).length > 0) {
       prevText = result.En_text;
-      if (prevText.length < 1700) {
-        if (formatList.length > 0) {
-          outPutText = prevText.concat('\n', formatList);
-        } else {
-          outPutText = prevText.concat('\n', selectedTextToString);
-        }
 
-        nbTotalText = prevText.length + outPutText.length;
-        if (nbTotalText < 1700) {
-          chrome.storage.sync.set({ En_text: outPutText }, () =>
-            console.log(outPutText)
-          );
-        } else {
-          alert(`Nombre de caractère pour idp text EN  atteint ${nbTotalText}`);
-        }
+      // clacul sum of sotred data + selected data
+      nbTotalText = outPutText.length + prevText.length;
+
+      if (nbTotalText < 1700) {
+        outPutText = prevText.concat('\n', outPutText);
+        chrome.storage.sync.set({ En_text: outPutText }, () =>
+          console.log(outPutText)
+        );
+      } else {
+        alert(`Nombre de caractère pour idp text EN  atteint ${nbTotalText}`);
       }
     } else {
-      if (formatList.length > 0) {
-        chrome.storage.sync.set({ En_text: formatList }, () =>
-          console.log(formatList)
-        );
-      }
+      // if there is data stored
       chrome.storage.sync.set({ En_text: outPutText }, () =>
         console.log(outPutText)
       );
